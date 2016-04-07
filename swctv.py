1d35ec403449054f162e2f7e38de186ffd368597
stefan.schwendeler <stefan.schwendeler@google.com>
2016-04-07 13:47:46 +0200

    *better icon resolution *stripped some code

--- swctv.py
+++ swctv.py
@@ -8,16 +8,17 @@ Allows to watch unencrypted Swisscom TV video streams with Kodi.
 import sys
 import os
 import urllib, urlparse
-#~ import collections
 import sqlite3
+#~ import collections
 
 import xbmcgui
 import xbmcplugin
 
-addon_handle = int(sys.argv[1])
-args = urlparse.parse_qs(sys.argv[2][1:])
+_basedir = os.path.dirname(__file__)
+_addon_handle = int(sys.argv[1])
+_args = urlparse.parse_qs(sys.argv[2][1:])
 
-xbmcplugin.setContent(addon_handle, 'movies')
+xbmcplugin.setContent(_addon_handle, 'movies')
 
 def build_url(query):
     return sys.argv[0] + '?' + urllib.urlencode(query)
@@ -36,61 +37,58 @@ def prefered_url(channels):
 
 
 # the predefined folders
-res_folders = ('SD', 'HD', 'UHD')
-root_folders = dict()
-#~ root_folders = collections.OrderedDict()
-root_folders.update((
-    ('Language', ('SELECT * FROM swc_tv WHERE language=?', lambda a: a)),
-    ('Resolution', ('SELECT * FROM swc_tv WHERE resolution=?', res_folders.index)),
-#~ ('Favorites', ('SELECT * FROM swc_fav ORDER BY count DESC LIMIT 1,10',)),
-#~ ('Last Seen', ('SELECT * FROM swc_fav ORDER BY date DESC LIMIT 1,10',)),
+media_folders = dict()
+#~ media_folders = collections.OrderedDict()
+media_folders.update((
+    ('Language', ("SELECT distinct upper(language) FROM swc_tv where language <> '' ORDER BY language ASC",
+                 ('SELECT * FROM swc_tv WHERE language=lower(?)', lambda a: a)
+                 )
+    ),
+    ('Resolution', (('SD', 'HD', 'UHD'),
+                   ('SELECT * FROM swc_tv WHERE resolution=?', ('SD', 'HD', 'UHD').index)
+                   )
+    ),
 ))
 
-db = sqlite3.connect(os.path.join(os.path.dirname(__file__), u'swctv.db'))
+db = sqlite3.connect(os.path.join(_basedir, u'swctv.db'))
 
-folder = args.get('folder', [None])[0]
-entry = args.get('entry', [None])[0]
+folder = _args.get('folder', [None])[0]
+entry = _args.get('entry', [None])[0]
 if folder is None:
     # root folder
-    for elem in root_folders:
+    for elem in media_folders:
         kodi_url = build_url({'folder': 'root', 'entry': elem})
         kodi_li = xbmcgui.ListItem(elem, iconImage='DefaultFolder.png')
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=kodi_url,
+        xbmcplugin.addDirectoryItem(handle=_addon_handle, url=kodi_url,
                                     listitem=kodi_li, isFolder=True)
-    xbmcplugin.endOfDirectory(addon_handle)
+    xbmcplugin.endOfDirectory(_addon_handle)
 
 
 elif folder == 'root':
     # sub folder
-    if entry == 'Resolution':
-        for elem in res_folders:
-            kodi_url = build_url({'folder': entry, 'entry': elem})
-            kodi_li = xbmcgui.ListItem(elem, iconImage='DefaultFolder.png')
-            xbmcplugin.addDirectoryItem(handle=addon_handle, url=kodi_url,
-                                        listitem=kodi_li, isFolder=True)
-        xbmcplugin.endOfDirectory(addon_handle)
-
-    elif entry == 'Language':
+    subfolder = media_folders[entry][0]
+    if isinstance(subfolder, str):
         cur = db.cursor()
-        cur.execute("SELECT distinct language FROM swc_tv where language <> '' ORDER BY language ASC")
+        cur.execute(subfolder)
+        subfolder = [d[0] for d in cur.fetchall()]
 
-        for lang in cur.fetchall():
-            kodi_url = build_url({'folder': entry, 'entry': lang[0]})
-            kodi_li = xbmcgui.ListItem(lang[0].upper(), iconImage='DefaultFolder.png')
-            xbmcplugin.addDirectoryItem(handle=addon_handle, url=kodi_url,
+    for elem in subfolder:
+        kodi_url = build_url({'folder': entry, 'entry': elem})
+        kodi_li = xbmcgui.ListItem(elem, iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(handle=_addon_handle, url=kodi_url,
                                         listitem=kodi_li, isFolder=True)
-        xbmcplugin.endOfDirectory(addon_handle)
+    xbmcplugin.endOfDirectory(_addon_handle)
 
 
-elif folder in root_folders:
-    query = root_folders[folder][0]
-    param = root_folders[folder][1](entry)
+elif folder in media_folders:
+    query, post_action = media_folders[folder][-1]
+    param = post_action(entry)
 
     cur = db.cursor()
     cur.execute(query, (param,))
     for stream_url, name, language, desc, resolution, thumb in prefered_url(cur.fetchall()):
         if thumb:
-            thumb_path = os.path.join(os.path.dirname(__file__), 'resources', 'media', thumb)
+            thumb_path = os.path.join(_basedir, 'resources', 'media', thumb)
             if not os.path.exists(thumb_path):
                 # kodi can't handle "memory" images, so create a folder and extract the image from DB into the filesystem
                 if not os.path.exists(os.path.dirname(thumb_path)):
@@ -103,9 +101,10 @@ elif folder in root_folders:
             kodi_li = xbmcgui.ListItem(name, iconImage=thumb_path, thumbnailImage=thumb_path)
         else:
             kodi_li = xbmcgui.ListItem(name, iconImage='DefaultVideo.png')
-        kodi_li.setInfo(type="Video", infoLabels={"Title": name, 'Description': desc, 'Language':language})
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=stream_url, listitem=kodi_li)
 
-    xbmcplugin.endOfDirectory(addon_handle)
+        kodi_li.setInfo(type="Video", infoLabels={"Title": name, 'plot': desc})
+        xbmcplugin.addDirectoryItem(handle=_addon_handle, url=stream_url, listitem=kodi_li)
+
+    xbmcplugin.endOfDirectory(_addon_handle)
 
 db.close()
