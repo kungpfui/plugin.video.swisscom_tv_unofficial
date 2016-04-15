1fbebf9cf6534196f26f2ae9c588be5296486163
stefan.schwendeler <stefan.schwendeler@google.com>
2016-04-15 20:59:43 +0200

    added "prefered resolution" setting and filter. Does not work always but most of the time. :-)

--- swctv.py
+++ swctv.py
@@ -9,10 +9,13 @@ import sys
 import os
 import urllib, urlparse
 import sqlite3
-#~ import collections
 
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
+
+
+__settings__ = xbmcaddon.Addon(id='plugin.video.swisscom_tv_unofficial')
 
 _basedir = os.path.dirname(__file__)
 _addon_handle = int(sys.argv[1])
@@ -22,6 +25,22 @@ xbmcplugin.setContent(_addon_handle, 'movies')
 
 def build_url(query):
     return sys.argv[0] + '?' + urllib.urlencode(query)
+
+def word_replace(s, replace):
+    words = s.split(' ')
+    for i, part in enumerate(words):
+        if part in replace:
+            if isinstance(replace, (list,tuple)):
+                words[i] = None
+            elif isinstance(replace, dict):
+                words[i] = replace[part]
+
+    # remove None values
+    while words.count(None):
+        words.remove(None)
+
+    return ' '.join(words)
+
 
 def prefered_url(channels):
     """remove double entries. prefere urls which use port 10000"""
@@ -36,16 +55,40 @@ def prefered_url(channels):
     return ch
 
 
+def resolution_filter(channels):
+    """Try to remove not prefered channel resolutions."""
+    pref_res = __settings__.getSetting("prefered_resolution")
+
+    names = []
+    for url, name, language, desc, resolution, thumb in channels:
+        names.append(name.lower())
+
+    ch = []
+    for url, name, language, desc, resolution, thumb in channels:
+        if pref_res == 'SD' and resolution != 0:
+            found = word_replace(name.lower(), ('hd', 'uhd', '4k', '4k1'))
+            if found in names: continue
+        elif pref_res == 'HD' and resolution != 1:
+            found = word_replace(name.lower(), ('uhd', '4k', '4k1')) + ' hd'
+            if found in names: continue
+        elif pref_res == 'UHD' and resolution != 2:
+            found = word_replace(name.lower(), ('hd',)) + ' uhd'
+            if found in names: continue
+
+        ch.append((url, name, language, desc, resolution, thumb))
+    return ch
+
+
 # the predefined folders
 media_folders = dict()
 #~ media_folders = collections.OrderedDict()
 media_folders.update((
     ('Language', ("SELECT distinct upper(language) FROM swc_tv where language <> '' ORDER BY language ASC",
-                 ('SELECT * FROM swc_tv WHERE language=lower(?)', lambda a: a)
+                 ('SELECT * FROM swc_tv WHERE language=lower(?)', lambda a: a, resolution_filter)
                  )
     ),
     ('Resolution', (('SD', 'HD', 'UHD'),
-                   ('SELECT * FROM swc_tv WHERE resolution=?', ('SD', 'HD', 'UHD').index)
+                   ('SELECT * FROM swc_tv WHERE resolution=?', ('SD', 'HD', 'UHD').index, lambda a: a)
                    )
     ),
 ))
@@ -81,12 +124,12 @@ elif folder == 'root':
 
 
 elif folder in media_folders:
-    query, post_action = media_folders[folder][-1]
+    query, post_action, post_filter = media_folders[folder][-1]
     param = post_action(entry)
 
     cur = db.cursor()
     cur.execute(query, (param,))
-    for stream_url, name, language, desc, resolution, thumb in prefered_url(cur.fetchall()):
+    for stream_url, name, language, desc, resolution, thumb in post_filter(prefered_url(cur.fetchall())):
         if thumb:
             thumb_path = os.path.join(_basedir, 'resources', 'media', thumb)
             if not os.path.exists(thumb_path):
